3f967ca3b515c0ac1a79677b1946d6e6a873c1de
prabhuramgr <prabhuramgr@gmail.com>
2016-06-16 00:00:05 -0500

    NLP working copy

---
+++ src/Controller/Mycontroller.java
@@ -0,0 +1,50 @@
+package Controller;
+
+import java.awt.Button;
+import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBoxBuilder;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+
+public class Mycontroller implements Initializable
+{
+	
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
